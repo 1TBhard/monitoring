3b7338d6e90fe335a418a9ecb5603f5b60960fb0
1TBhard <was1383@naver.com>
2023-02-14 21:27:38 +0900

    setTimezone 추가 및 반영

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Dashboard from "src/page/Dashboard";
 import ThemeProvider from "src/component/main/ThemeProvider";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "src/hook/store";
+import "src/util/setTimezone";
 
 function App() {
 	return (
