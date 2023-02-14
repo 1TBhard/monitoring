93d16ed313816b3a566f5db1990d6e0513a8ef34
1TBhard <was1383@naver.com>
2023-02-14 16:51:03 +0900

    Proejct 타입 추가

--- src/api/project/getProject.ts
+++ src/api/project/getProject.ts
@@ -1,8 +1,9 @@
 import openWhatapAPI from "src/api/openWhatapAPI";
 import { API_URL } from "src/const/API_URL";
+import Project from "src/type/Project";
 
 export default async function getProject() {
 	const url = API_URL.PROJECT.INFO;
 
-	return openWhatapAPI.get(url);
+	return openWhatapAPI.get<Project>(url);
 }
