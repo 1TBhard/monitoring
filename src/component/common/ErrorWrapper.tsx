a6a3624b94ea1ca54726c720a4076a3f3527f086
1TBhard <was1383@naver.com>
2023-02-16 00:21:48 +0900

    ErrorWrapper 추가

---
+++ src/component/common/ErrorWrapper.tsx
@@ -0,0 +1,22 @@
+import { Spin, SpinProps } from "antd";
+
+interface ErrorWrapperProps
+	extends Omit<SpinProps, "indicator" | "tip" | "spinning"> {
+	isError?: boolean;
+	errorMessage: string;
+}
+
+export default function ErrorWrapper({
+	isError = false,
+	errorMessage,
+	...restProps
+}: ErrorWrapperProps) {
+	return (
+		<Spin
+			spinning={isError}
+			indicator={undefined}
+			tip={errorMessage}
+			{...restProps}
+		/>
+	);
+}
---
+++ src/const/MESSAGE.ts
@@ -0,0 +1,3 @@
+export const LOADING = "로딩중..";
+
+export const LOAD_FAIL = "에러가 발생하였습니다. 잠시후 다시 시도해주세요.";
