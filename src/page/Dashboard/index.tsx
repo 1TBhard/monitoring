93961a8fa964fd658686ca76394d5e73507d8cd1
1TBhard <was1383@naver.com>
2023-02-16 00:24:38 +0900

    인포메틱스 위젯 최적화 및 에러 추가

--- src/page/Dashboard/index.tsx
+++ src/page/Dashboard/index.tsx
@@ -1,31 +1,15 @@
-import dayjs from "dayjs";
-import MainLayout from "src/component/common/MainLayout";
-import ActiveUserWidget from "src/component/main/widget/ActiveUserWidget";
-import InfomaticWidget from "src/component/main/widget/InfomaticWidget";
-import SqlErrorWidget from "src/component/main/widget/SqlErrorWidget";
-import useProject from "src/hook/project/useProject";
-import useSpot from "src/hook/spot/useSpot";
-import useActiveUserByHour from "src/hook/statistics/useActiveUserByHour";
 import * as Styled from "./Styled.Dashboard";
+import InfomaticWidget from "src/component/main/widget/InfomaticWidget";
+import MainLayout from "src/component/common/MainLayout";
+import useProject from "src/hook/project/useProject";
 
 export default function Dashboard() {
 	const { projectName } = useProject();
-	const { spotItemList } = useSpot();
-
-	const stime = dayjs().startOf("day").toDate().getTime();
-	const etime = dayjs().endOf("day").toDate().getTime();
-
-	const { activeUserList } = useActiveUserByHour({
-		etime,
-		stime,
-	});
 
 	return (
 		<MainLayout title={projectName}>
 			<Styled.GridFrame>
-				<InfomaticWidget itemList={spotItemList} />
-				<ActiveUserWidget activeUserList={activeUserList} />
-				<SqlErrorWidget stime={stime} etime={etime} />
+				<InfomaticWidget />
 			</Styled.GridFrame>
 		</MainLayout>
 	);
