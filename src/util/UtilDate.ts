8c87a6a85766fb05528d5e3810cd2653a333390f
1TBhard <was1383@naver.com>
2023-02-20 14:08:14 +0900

    UtilDate - getYesterdayStimeEtime 추가

--- src/util/UtilDate.ts
+++ src/util/UtilDate.ts
@@ -7,9 +7,23 @@ export default class UtilDate {
 		return day.format("HH:mm");
 	}
 
+	/**
+	 * @description 00:00 ~ 현재시간을 반환한다.
+	 */
 	static getTodayStimeEtime() {
 		const stime = dayjs().startOf("day").toDate().getTime();
 		const etime = dayjs().endOf("day").toDate().getTime();
+
+		return { stime, etime };
+	}
+
+	/**
+	 * @description 어제 00:00 ~ 23:59을 반환한다.
+	 */
+	static getYesterdayStimeEtime(dateNumber: number) {
+		const yesterday = dayjs(new Date(dateNumber)).add(-1, "day");
+		const stime = yesterday.startOf("day").toDate().getTime();
+		const etime = yesterday.endOf("day").toDate().getTime();
 
 		return { stime, etime };
 	}
