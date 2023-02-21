39e78fa3c9a39b8f2f3e43f842377526296a4ca9
1TBhard <was1383@naver.com>
2023-02-21 16:04:49 +0900

    인포매틱스 위젯 데이터 호출부 Content로 이관

--- src/component/main/widget/InfomaticWidget/index.tsx
+++ src/component/main/widget/InfomaticWidget/index.tsx
@@ -1,19 +1,12 @@
-import Widget from "src/component/common/Widget";
-import InfomaticWidgetContent from "src/component/main/widget/InfomaticWidget/InfomaticWidgetContent";
-import useSpot from "src/hook/spot/useSpot";
 import * as Styled from "./Styled.InfomaticWidget";
+import InfomaticWidgetContent from "src/component/main/widget/InfomaticWidget/InfomaticWidgetContent";
+import Widget from "src/component/common/Widget";
 
 export default function InfomaticWidget() {
-	const { spotItemList, isLoading, isError } = useSpot();
-
 	return (
 		<Widget title='인포매틱스'>
 			<Styled.Frame>
-				<InfomaticWidgetContent
-					spotItemList={spotItemList}
-					isLoading={isLoading}
-					isError={isError}
-				/>
+				<InfomaticWidgetContent />
 			</Styled.Frame>
 		</Widget>
 	);
