9468fae0632f22e91abc983c9a8c4beb50a20fad
1TBhard <was1383@naver.com>
2023-02-22 20:26:57 +0900

    UtilDate의 메서드 추가 - getDateBeforeMinutes, getStimeEtimeBySecInterval, msToSecString

--- src/util/UtilDate.ts
+++ src/util/UtilDate.ts
@@ -1,4 +1,5 @@
 import dayjs from "dayjs";
+import UtilNumber from "src/util/UtilNumber";
 
 export default class UtilDate {
 	static dateBumberToHHmm(dateNumber: number) {
@@ -49,5 +50,45 @@ export default class UtilDate {
 		const roundedSec = Math.floor(dateSec / intervalSec) * intervalSec;
 
 		return dayjs(date).set("seconds", roundedSec).toDate();
+	}
+
+	/**
+	 * @description Date를 원하는 분 만큼 뺀 Date를 반환
+	 */
+	static getDateBeforeMinutes(date: Date, minutes: number) {
+		return dayjs(date).add(-minutes, "minutes").toDate();
+	}
+
+	/**
+	 * @description startDate ~ endDate 를 sec만큼 나눌 때의 [{stime, etime}] 리스트 반환
+	 */
+	static getStimeEtimeBySecInterval(
+		startDate: Date,
+		endDate: Date,
+		sec: number
+	): { stime: number; etime: number }[] {
+		const start = dayjs(startDate);
+		const end = dayjs(endDate).add(-sec, "seconds");
+
+		const result = [];
+		for (
+			let curStart = start;
+			!curStart.isAfter(end);
+			curStart = curStart.add(sec, "seconds")
+		) {
+			result.push({
+				stime: curStart.toDate().getTime(),
+				etime: dayjs(curStart).add(sec, "seconds").toDate().getTime(),
+			});
+		}
+
+		return result;
+	}
+
+	/**
+	 * @description 밀리세컨드를 초로 변환한 String 반환
+	 */
+	static msToSecString(ms: number | string) {
+		return `${UtilNumber.toLocaleString(Math.round(Number(ms) / 10) / 100)}s`;
 	}
 }
